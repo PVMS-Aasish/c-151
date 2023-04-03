62fca36b1a1c79e755c9f5fe96919a69535c83d9
PVMS-Aasish <73524800+PVMS-Aasish@users.noreply.github.com>
2023-04-03 15:02:09 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,11 @@
+AFRAME.registerComponent('log',{
+    schema:{
+        message:{ type: 'string', default: 'Hello, World'}
+    },
+
+    init: function(){
+        console.log(this.data.message);
+    }
+
+
+})
